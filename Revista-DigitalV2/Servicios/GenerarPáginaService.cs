be3232d14a21443467e44871ff34e376ba466e39
jorge camara <jorcamher@alu.edu.gva>
2023-02-13 11:33:33 +0100

    Servicio generacion html acabado

--- Revista-DigitalV2/Servicios/GenerarPáginaService.cs
+++ Revista-DigitalV2/Servicios/GenerarPáginaService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,28 +14,31 @@ namespace Revista_DigitalV2.Servicios
         public void GenerarHTML(ObservableCollection<Articulo> articulos)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("<!DOCTYPE html>");
-            sb.Append("<html lang = \"en\" xmlns = \"http://www.w3.org/1999/xhtml\">");
-            sb.Append("<head>");
-            sb.Append("<meta charset = \"utf - 8\" />");
-            sb.Append("<title></title>");
-            sb.Append("</head>");
-            sb.Append("<body>");
+            sb.Append("<!DOCTYPE html>\n");
+            sb.Append("<html lang = \"en\" xmlns = \"http://www.w3.org/1999/xhtml\">\n");
+            sb.Append("<head>\n");
+            sb.Append("<meta charset = \"utf - 8\" />\n");
+            sb.Append("<title></title>\n");
+            sb.Append("</head>\n");
+            sb.Append("<body>\n");
 
             foreach(Articulo articuloSecciones in articulos)
             {
                 string seccionActual = articuloSecciones.Seccion;
-                sb.Append("<h2>" + seccionActual + "</h2>");
+                sb.Append("<h2>" + seccionActual + "</h2>\n");
 
                 foreach(Articulo articulo in articulos)
                 {
-                    sb.Append("<a href=\"" + articulo.URL + "\"/>");
+                    sb.Append("<a href=\"" + articulo.URL + "\"/>\n");
                 }
 
             }
-            sb.Append("</body>");
-            sb.Append("</html> ");
+            sb.Append("</body>\n");
+            sb.Append("</html>\n");
 
+            StreamWriter sw = new StreamWriter("index.html");
+            sw.WriteLine(sb.ToString());
+            sw.Close();
         }
     }
 }
